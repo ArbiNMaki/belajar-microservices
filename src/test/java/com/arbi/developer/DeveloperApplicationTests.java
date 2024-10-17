ffdfb970b620859fa3b4bb962b0e19688169e168
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-10-17 20:49:26 +0700

    quiz app monolith 1

---
+++ src/test/java/com/arbi/developer/DeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbi.developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
